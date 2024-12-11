a8606f43bf1c03fd1ffd689b7a367773c61bb3b7
soju <underrated@gmail.com>
2024-12-10 17:11:24 -0800

    Update omniscientPlugin.ts

--- agent/src/plugins/omniscient/omniscientPlugin.ts
+++ agent/src/plugins/omniscient/omniscientPlugin.ts
@@ -2,19 +2,22 @@ import { Plugin } from "@ai16z/eliza";
 import { contextSummaryAction } from "./actions/contextSummaryAction";
 import { projectSummaryAction } from "./actions/projectSummaryAction";
 import { summaryAction } from "./actions/summaryAction";
+import { importanceEvaluator } from "./evaluators/importanceEvaluator";
 import { projectEvaluator } from "./evaluators/projectEvaluator";
+import { roleEvaluator } from "./evaluators/roleEvaluator";
 import { omniscientProvider } from "./providers/omniscientProvider";
 
 export const omniscientPlugin: Plugin = {
     name: "OMNISCIENT",
     description:
         "Maintains project overview and guides team focus by filtering noise and highlighting priorities",
-    evaluators: [projectEvaluator],
+    evaluators: [projectEvaluator, importanceEvaluator, roleEvaluator],
     providers: [omniscientProvider],
     actions: [
         summaryAction,
         projectSummaryAction,
         contextSummaryAction,
+
         // priorityFilterAction,
         // contextSwitchAction,
         // focusMetricsAction,
